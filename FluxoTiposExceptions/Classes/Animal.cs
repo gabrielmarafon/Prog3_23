58ebc9445aeb38e4669314b95ce242ddc7cb9996
Gabriel Marafon <gabrielmarafon5@gmail.com>
2023-04-10 22:07:30 -0300

    Aula 10/04/2023

---
+++ FluxoTiposExceptions/Classes/Animal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluxoTiposExceptions.Classes
+{
+    //Animal é um super class, uma classe que contém atributos e especificações genéricas.
+    public class Animal
+    {
+        public string Name;
+        public DateTime Born;
+        public byte Legs;
+    }
+}
--- FluxoTiposExceptions/Program.cs
+++ FluxoTiposExceptions/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Operadores Unários
+using FluxoTiposExceptions.Classes;
+
 int x = 5;
 int incrementoPosfixado = x++; //6
 incrementoPosfixado++; //x++; x = x + 1
@@ -36,9 +38,74 @@ WriteLine($"a    |{a & a, -5} | {a & b, -5}");
 
 static bool FacaAlgo()
 {
-    WriteLine("fazenfo alguma coisa");
+    WriteLine("fazendo alguma coisa");
     return true;
 }
 WriteLine();
 WriteLine($"a & facaAlgo() = {a & FacaAlgo()}");
 WriteLine($"b & facaAlgo() = {b & FacaAlgo()}");
+
+//-------------------------------------------------
+
+Animal[] animals = new Animal[]
+{
+    new Cat{
+        Name = "Craudio"
+        , Born = new(year: 2022, month: 8, day: 23)
+        , Legs = 4
+        , IsDomestic = true
+    },
+    new Animal{
+        Name = "Eu mesmo"
+        ,Born = new(year: 1984, month:7, day:5)
+        ,Legs = 2
+    },
+
+    new Cat{
+        Name = "Darci"
+        , Born = new(year: 1994, month: 6, day: 12)
+        , Legs = 3
+        , IsDomestic = true
+    },
+
+    new Spider{
+        Name = "Pernuda"
+        , Born = DateTime.Today
+        , IsPoisonous = true
+        , Legs = 8
+    },
+
+    new Cat{
+        Name = "Champola"
+        , Born = DateTime.Today
+        , Legs = 3
+        , IsDomestic = false
+    },
+};
+
+// Laços de Repetição
+// 3 tipos de variáveis para controle de repetição
+//1 flag - Bandeirinha, booleano que indica se para ou se continua
+//2 counter - Contador, conta quantas vezes a repetição ocorreu 
+//3 acumulador - Permite acumular valores a cada interação
+
+//laço for 
+for (int i = 0; i < animals.Length; i++)
+{
+    string message; 
+    message = $"Nome: {animals[i].Name} ";
+    message += $"Data Nasc: {animals[i].Born.ToShortDateString()}";
+    Console.WriteLine(message);
+}
+
+//laço de repetição para cada, dispensa contador e acumulador
+Console.WriteLine("\n---FOREACH---\n");
+foreach( Animal animal in animals )
+{
+    string message = string.Empty;
+    if(animal != null)
+    {
+        message += $"Nome: {animal.Name}";
+    }
+    Console.WriteLine(message);
+}
