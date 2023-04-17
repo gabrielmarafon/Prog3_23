38bc0cf34c51f9c7e4657bff0f7ef4361d717526
Gabriel Marafon <gabrielmarafon5@gmail.com>
2023-04-17 19:27:48 -0300

    AtivPessoas

---
+++ AtivPessoas/Classes/Manager.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtivPessoas.Classes
+{
+    public class Manager : Person
+    {
+        public int SubordinateSectors;
+        public int Wage;
+    }
+}
---
+++ AtivPessoas/Classes/Supervisor.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtivPessoas.Classes
+{
+    public class Supervisor : Person
+    {
+        public int SubordinateEmployees;
+        public int Wage;
+    }
+}
